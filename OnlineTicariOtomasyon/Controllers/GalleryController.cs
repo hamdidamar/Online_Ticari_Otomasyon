0b200cb0d7ca7429de2cf803ddcb29dbd9453980
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2022-03-24 22:48:02 +0300

    Galeri sayfası eklendi

---
+++ OnlineTicariOtomasyon/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Classes;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class GalleryController : Controller
+    {
+        Context ctx = new Context();
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            var products = ctx.Products.Where(x => x.IsActive).ToList();
+            return View(products);
+        }
+    }
+}
