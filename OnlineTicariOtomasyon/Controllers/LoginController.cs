289893d17d17ffa550227ae14678d92c87df8626
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2022-03-29 21:19:29 +0300

    kayıt işlemi eklendi

--- OnlineTicariOtomasyon/Controllers/LoginController.cs
+++ OnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineTicariOtomasyon.Models.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,25 @@ namespace OnlineTicariOtomasyon.Controllers
 {
     public class LoginController : Controller
     {
+        Context ctx = new Context();
         // GET: Login
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public PartialViewResult Register()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Customer customer)
+        {
+            customer.IsActive = true;
+            ctx.Customers.Add(customer);
+            ctx.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- OnlineTicariOtomasyon/Models/Classes/Customer.cs
+++ OnlineTicariOtomasyon/Models/Classes/Customer.cs
@@ -13,6 +13,7 @@ namespace OnlineTicariOtomasyon.Models.Classes
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Mail { get; set; }
+        public string Password { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
         public bool IsActive { get; set; }
