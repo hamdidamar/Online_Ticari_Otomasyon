82ba85eaef95cc56480f5a769f69f73806069b35
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2022-04-02 21:33:55 +0300

    qr kodu oluşturma sayfası eklendi

---
+++ OnlineTicariOtomasyon/Controllers/QRController.cs
@@ -0,0 +1,37 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static QRCoder.QRCodeGenerator;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class QRController : Controller
+    {
+        // GET: QR
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string code)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator generator = new QRCodeGenerator();
+                QRCode qRCode = generator.CreateQrCode(code, ECCLevel.Q);
+                using (Bitmap img = qRCode.GetGraphic(10))
+                {
+                    img.Save(ms, ImageFormat.Png);
+                    ViewBag.qrImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return View();
+        }
+    }
+}
