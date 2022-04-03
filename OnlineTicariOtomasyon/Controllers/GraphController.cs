bf0035a86ecf85b49e58cff5a93412f710b9deae
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2022-04-03 14:03:00 +0300

    controller grafik oluşturma

--- OnlineTicariOtomasyon/Controllers/GraphController.cs
+++ OnlineTicariOtomasyon/Controllers/GraphController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace OnlineTicariOtomasyon.Controllers
@@ -12,6 +13,16 @@ namespace OnlineTicariOtomasyon.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult CategoryAndProductCountGraph()
+        {
+            var graph = new Chart(600, 600);
+            graph.AddTitle("Kategori - Ürün Stok Grafiği")
+                 .AddLegend("Stok")
+                 .AddSeries("Kategoriler", xValue: new[] { "mobilya", "beyaz eşya", "teknoloji" }, yValues: new[] { 76, 97, 138 })
+                 .Write();
+            return File(graph.ToWebImage().GetBytes(), "image/jpeg");
         }
     }
 }
