2bfa5c65b1cd022ffd7e80794fb89290c479d484
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2022-04-04 22:58:28 +0300

    giden mesajlar eklendi

--- OnlineTicariOtomasyon/Controllers/CustomerPanelController.cs
+++ OnlineTicariOtomasyon/Controllers/CustomerPanelController.cs
@@ -45,5 +45,19 @@ namespace OnlineTicariOtomasyon.Controllers
             ViewBag.deletedCount = deletedCount;
             return View(messages);
         }
+
+        public ActionResult SendMessages()
+        {
+            var CustomerId = int.Parse(Session["CustomerId"].ToString());
+            var customer = ctx.Customers.Where(x => x.IsActive && x.CustomerId == CustomerId).FirstOrDefault();
+            var messages = ctx.Messages.Where(x => x.IsActive && x.From == customer.Mail).ToList();
+            var inboxCount = ctx.Messages.Where(x => x.IsActive && x.To == customer.Mail).Count();
+            var sendCount = ctx.Messages.Where(x => x.IsActive && x.From == customer.Mail).Count();
+            var deletedCount = ctx.Messages.Where(x => !x.IsActive && x.To == customer.Mail).Count();
+            ViewBag.inboxCount = inboxCount;
+            ViewBag.sendCount = sendCount;
+            ViewBag.deletedCount = deletedCount;
+            return View(messages);
+        }
     }
 }
