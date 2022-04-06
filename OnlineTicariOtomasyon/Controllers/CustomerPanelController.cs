be14dfa4524adeac19037e6f39a9f6508b261e11
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2022-04-06 20:52:06 +0300

    Çıkış işlemleri eklendi

--- OnlineTicariOtomasyon/Controllers/CustomerPanelController.cs
+++ OnlineTicariOtomasyon/Controllers/CustomerPanelController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace OnlineTicariOtomasyon.Controllers
 {
@@ -136,6 +137,12 @@ namespace OnlineTicariOtomasyon.Controllers
             return View(operations);
         }
 
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
 
     }
 }
