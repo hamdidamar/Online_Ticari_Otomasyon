871fe35f6b08fd899222aa3d85d4d2fe2cc2cca0
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2022-04-09 10:46:48 +0300

    hata yakalama işlemleri

---
+++ OnlineTicariOtomasyon/Controllers/ErrorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Page404()
+        {
+            Response.TrySkipIisCustomErrors = true;
+            return View();
+        }
+
+        public ActionResult E400()
+        {
+            Response.StatusCode = 400;
+            Response.TrySkipIisCustomErrors = true;
+            return View("PageError");
+        }
+
+        public ActionResult E403()
+        {
+            Response.StatusCode = 403;
+            Response.TrySkipIisCustomErrors = true;
+            return View("PageError");
+        }
+
+        public ActionResult E404()
+        {
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
+            return View("PageError");
+        }
+    }
+}
