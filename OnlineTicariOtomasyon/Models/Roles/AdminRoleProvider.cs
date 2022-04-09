32d82bf0b0d27463dac0347b841d63e924a880e4
Hamdi DAMAR <39102004+hamdidamar@users.noreply.github.com>
2022-04-09 13:00:08 +0300

    rol oluşturma işlemleri eklendi

---
+++ OnlineTicariOtomasyon/Models/Roles/AdminRoleProvider.cs
@@ -0,0 +1,65 @@
+﻿using OnlineTicariOtomasyon.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace OnlineTicariOtomasyon.Models.Roles
+{
+    public class AdminRoleProvider : RoleProvider
+    {
+        Context ctx = new Context();
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            return new string[] { ctx.Admins.Where(x => x.Username == username).FirstOrDefault().Role };
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
